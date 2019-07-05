46d4c2e9c8e3c0f2fa9981c76cef73bda3df4cea
Zunaira Riaz <zunaira990@gmail.com>
2019-07-05 17:58:39 +0500

    Changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,45 @@
 class UsersController < ApplicationController
-
+include UsersHelper
 def index
-@users = User.all
+@user = User.all
 end
 
 
 def show
-@users = User.find(params[:id])
+@user = User.find(params[:id])
 end
 
+
+def new
+@user = User.new
 end
 
+def create 
+@user = User.new(user_params)
+@user.save
+redirect_to users_path(@user)
+end
+
+
+
+
+def destroy
+@user = User.find(params[:id])
+@user.destroy 
+redirect_to users_path
+end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  @user.update(user_params)
+
+  redirect_to user_path(@user)
+end
+
+
+
+end
--- db/migrate/20190705093245_create_users.rb
+++ db/migrate/20190705093245_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|    
+    create_table :user do |t|    
       t.string :name
       t.integer :age
       t.text :address 	
